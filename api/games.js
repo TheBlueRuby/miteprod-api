a10d9a8f8f03cd150a5651982db8167a1d25904d
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2022-10-23 22:45:28 +1100

    fixed error handling and fixed file creation

--- api/games.js
+++ api/games.js
@@ -49,35 +49,35 @@ router.post("/:id", (req, res) => {
     }
 
     if (pwd == password) {
-        fs.mkdir("/tmp/api/data/games", { recursive: true }, (err) => {
-            console.error(err);
-            res.status(500).json({
-                status: "500",
-                message: `Error, Could Not Create /tmp/api/data/games/ !`,
-            });
+        let thereAreErrors = false;
+        let error = '';
+
+        fs.mkdirSync("/tmp/api/data/games/", { recursive: true }, (err) => {
+            error = err;
+            thereAreErrors = true;
         });
 
-        fs.writeFile(`/tmp/api/data/games/${id}.json`, JSON.stringify(req.body), (err) => {
-            console.error(err);
-            res.status(500).json({
-                status: "500",
-                message: `Error, Could Not Create /tmp/api/data/games/${id}.json !`,
-            });
+        fs.writeFileSync(`/tmp/api/data/games/${id}.json`, JSON.stringify(req.body), (err) => {
+            error = err;
+            thereAreErrors = true;
         });
 
-        res.status(201).send({
-            id: id,
-        });
+        if (thereAreErrors) {
+            console.error(error);
+            res.status(500).json({
+                status: "500",
+                message: `Error, ${error}`,
+            });
+        } else {
+            res.status(201).send({
+                id: id,
+            });
+        }
     } else if (pwd) {
         res.status(403).send({ message: "Incorrect password" });
     } else {
         res.status(401).send({ message: "Please supply a password in the pwd field" });
     }
-
-    res.status(503).send({
-        status: "503",
-        message: "Unknown Error",
-    });
 });
 
 module.exports = router;
